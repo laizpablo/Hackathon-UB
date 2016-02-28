0ba348b6d05a1cdce5c1d42b971e571d5f964ded
laizpablo <laizpablo@hotmail.com>
2016-02-28 13:41:40 +0100

    Casual Jewel Mining Game
    Doesn't work properly

---
+++ Casual Jewel/Casual Jewel Mining Game/Assets/Scripts/Claw.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Claw : MonoBehaviour {
+
+    public Transform origin;
+    public float speed = 4f;
+    public Gun gun;
+    //public ScoreManager scoreManager;
+
+    private Vector3 _target;
+    private int _jewelValue = 100;
+    private GameObject _childObject;
+    private LineRenderer _lineRenderer;
+    private bool _hitJewel;
+    private bool _retracting; // for claw traveling back
+
+    // Use this for initialization
+    void Start () {
+        _lineRenderer = GetComponent<LineRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, _target, step);
+        _lineRenderer.SetPosition(0, origin.position);
+        _lineRenderer.SetPosition(1, transform.position);
+        if (transform.position == origin.position && _retracting){
+            gun.CollectedObject();
+            if (_hitJewel){
+                //scoreManager.AddPoints(_jewelValue);
+                _hitJewel = false;
+            }
+            Destroy(_childObject);
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void ClawTarget(Vector3 pos){
+        _target = pos;
+    }
+
+    void OnTriggerEnter(Collider other){
+        _retracting = true;
+        _target = origin.position;
+
+        if (other.gameObject.CompareTag("Jewel")){
+            _hitJewel = true;
+            _childObject = other.gameObject;
+            other.transform.SetParent(this.transform);
+        }
+
+        else if (other.gameObject.CompareTag("Rock")){
+            _childObject = other.gameObject;
+            other.transform.SetParent(this.transform);
+        }
+    }
+}
---
+++ Casual Jewel/Casual Jewel Mining Game/Assets/Scripts/Gun.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+    public GameObject claw; // where we will save the reference to the claw (gancho)
+    public bool isShooting; // True when we shoot
+    public Animator minerAnimator;
+    //public Claw clawScript;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1") && !isShooting){
+            LaunchClaw();
+        }
+    }
+
+    void LaunchClaw(){
+        isShooting = true;
+        minerAnimator.speed = 0;
+        RaycastHit hit; //Structure used to get information back raycast
+        Vector3 down = transform.TransformDirection(Vector3.down);
+
+        if (Physics.Raycast(transform.position, down, out hit, 100))
+        {
+            claw.SetActive(true);
+            //clawScript.ClawTarget(hit.point);
+        }
+    }
+
+    public void CollectedObject()
+    {
+        isShooting = false;
+        minerAnimator.speed = 1;
+    }
+}
