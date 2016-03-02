eb8cd2490b7c7397fa1cb6426d6f66c94376c4c0
laizpablo <laizpablo@hotmail.com>
2016-03-02 21:05:05 +0100

    Jump Player

---
+++ Jump Player/Assets/Scripts/jump.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jump : MonoBehaviour {
+
+    public float jumpHeight=500;
+    public bool isJumping = false;
+
+    private Rigidbody2D _rb2d; // Rigidbody2D physics component for 2D spritess
+
+    // Use this for initialization
+    void Start () {
+        _rb2d = GetComponent<Rigidbody2D>();
+        Debug.Log("Start");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping) {
+            _rb2d.AddForce(Vector2.up * jumpHeight);
+            isJumping = true;
+        }
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D col){
+        Debug.Log("collison");
+        if(col.gameObject.tag == "Ground"){
+            isJumping = false;
+        }
+    }
+
+}
